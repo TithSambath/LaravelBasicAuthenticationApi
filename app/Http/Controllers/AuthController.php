8413d3b957b5df2c6df9a01777686b4efb3ff348
TithSambath <dilytithsambath@gmail.com>
2022-08-14 17:41:16 +0700

    initial app

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function signUp(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user,
+        ], 201);
+    }
+
+    function signIn(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found',
+            ], 404);
+        }
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Password does not match',
+            ], 404);
+        }
+        $token = $user->createToken($request->device_name)->plainTextToken;
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'token' => $token,
+        ], 200);
+    }
+}
